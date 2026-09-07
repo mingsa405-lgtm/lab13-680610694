440ca8d9b33edb220770292bddd446629fd4dbd1
ming <mingsa405@gmail.com>
2026-09-07 13:31:23 +0700

    up

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,20 +1,23 @@
-
 import { createBrowserRouter } from "react-router";
 import MainLayout from "./layout/MainLayout";
-import ErrorPage from "./pages/ErrorPage";
-import TodolistPage from "./pages/TodolistPage";
-
+import HomePage from "./pages/HomePage";
+import TodolistPage from "./pages/TodolistPage"; // จะเปิดใช้ที่ STEP 8
+import ErrorPage from "./pages/ErrorPage"; // จะเปิดใช้ที่ STEP 8
 export const router = createBrowserRouter([
   {
     path: "/",
+    element: <MainLayout />, // โครงหน้า
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <HomePage /> }, // "/" -> HomePage อยู่ใน <Outlet/> ของ MainLayout
+    ],
+  },
+  {
+    path: "/my",
     element: <MainLayout />,
     errorElement: <ErrorPage />,
     children: [
-      {
-        index: true,
-        element: <TodolistPage />,
-      },
+       { path: "todolistpage", element: <TodolistPage /> },  // จะเปิดใช้ที่ STEP 8
     ],
   },
 ]);
-
